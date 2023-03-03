0f8f2a4d5e98b5c482c0a561140fd70f7bc495cd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-03 14:25:54 -0500

    Add reference to the add button

--- library.js
+++ library.js
@@ -1,5 +1,10 @@
+//References
+const addBtn = document.querySelector(".add-btn");
+
+//Constants/Variables
 const myLibrary = [];
 
+//Constructors
 function Book({ title = "", author = "", pages = 0, hasRead = false }) {
   this.title = title;
   this.author = author;
@@ -7,6 +12,7 @@ function Book({ title = "", author = "", pages = 0, hasRead = false }) {
   this.hasRead = hasRead;
 }
 
+//Functions
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
