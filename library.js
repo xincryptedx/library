44966edf89d7a7d1f0163e69b532d019c22085c1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-03 14:00:05 -0500

    Link .add-form to the proper grid area
    Also enabled Prettier. Format on save and the default formatter settings
    were changed to enable automatic formatting by Prettier.

--- library.js
+++ library.js
@@ -1,16 +1,16 @@
 const myLibrary = [];
 
-function Book({title = '', author = '', pages = 0, hasRead = false}) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
+function Book({ title = "", author = "", pages = 0, hasRead = false }) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+  myLibrary.push(book);
 }
 
 function loadLibrary() {
-    //Takes the info in library and displays it on the webpage
+  //Takes the info in library and displays it on the webpage
 }
