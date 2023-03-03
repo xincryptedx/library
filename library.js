71d66e66511b3a9a2f9ddce6f373e34c146de9cd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-02 19:39:04 -0500

    Add basic js skeleton logic

---
+++ library.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    //Constructor for Books
+}
+
+function addBookToLibrary() {
+    //Adds a new Book to the myLibrary array
+}
+
+function loadLibrary() {
+    //Takes the info in library and displays it on the webpage
+}
